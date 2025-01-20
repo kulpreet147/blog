94e8713fef09206e5a6676d2e05e621a8f19f805
kulpreet147 <kulpreet.singh@bentsys.com>
2025-01-20 18:23:22 +0530

    first cmt

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Users from "./Users";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <Users />
+      <Users />
+      <Users />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Users.js
@@ -0,0 +1,11 @@
+//functional component
+function Users() {
+    return(
+        <h1>
+            Users component
+        </h1>
+    );
+}
+
+export default Users;
+
